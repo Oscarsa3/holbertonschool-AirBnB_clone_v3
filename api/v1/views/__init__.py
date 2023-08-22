49ccbc85013bc6bf69b801d51333786d7856bf9e
Oscarsa3 <oscarsadev@gmail.com>
2023-08-22 14:29:49 -0700

    fix: circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 """Create a first blueprint"""
 from flask import Blueprint
-from api.v1.views.index import *
 
-
+"""new instance from Blueprint"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+"""wildcard import of everything in the package """
+from api.v1.views.index import *
