a04cd238090608ab8ad25e3b88e3d5c9b8871223
Oscarsa3 <oscarsadev@gmail.com>
2023-08-22 14:58:15 -0700

    feat: add a handler for 404 errors that returns a JSON-formatted 404 status code response.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,8 +11,14 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(exception=None):
-    """close"""
+    """close method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """return a json with 404 error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
