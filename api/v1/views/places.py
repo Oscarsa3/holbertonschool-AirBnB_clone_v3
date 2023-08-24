1191a17ff5d3815a7a8a4322c97199afd565e7cc
xiayudev <jctuesta7@gmail.com>
2023-08-24 00:01:03 -0500

    feat: add new route for places
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

---
+++ api/v1/views/places.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""Create a new route"""
+from flask import jsonify, abort, request
+from . import app_views
+from models import storage
+from models.user import User
+from models.city import City
+from models.place import Place
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
+def places_get(city_id):
+    """return json file with the dictionary of all places"""
+    obj_city = storage.get(City, city_id)
+    if obj_city is None:
+        abort(404)
+    list_places = []
+    for place in storage.all(Place).values():
+        if place.city_id == city_id:
+            list_places.append(place.to_dict())
+    return jsonify(list_places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
+def places_get_with_id(place_id):
+    """returns a json file with the dictionary of an object by its id"""
+    obj = storage.get(City, place_id)
+    if obj is None:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def places_post(city_id):
+    """returns a json file with the dictionary of a newly added object"""
+    obj_state = storage.get(City, city_id)
+    if obj_state is None:
+        abort(404)
+    data = request.get_json()
+    user_ids = [user.id for user in storage.all(User).values()]
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    elif 'user_id' not in data:
+        return jsonify({"error": "Missing user_id"}), 400
+    elif 'user_id' not in user_ids:
+        abort(404)
+    elif 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
+    new_place = Place(**data)
+    storage.new(new_place)
+    storage.save()
+    return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def places_delete(place_id):
+    """returns a json file with an empty dictionary if successfully deleted"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        abort(404)
+    storage.delete(obj)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def places_put(place_id):
+    """returns a json file with the dictionary of an
+    object that has been updated"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    ignore = ['id', 'created_at', 'updated_at', 'user_id', 'city_id']
+    for k, v in data.items():
+        if k not in ignore:
+            setattr(obj, k, v)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
