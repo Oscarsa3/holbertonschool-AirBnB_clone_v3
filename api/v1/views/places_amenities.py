320bfd9c99169fc0641fcb1d077befb895cb0eca
Oscarsa3 <oscarsadev@gmail.com>
2023-08-25 11:18:39 -0700

    fix: Improving methods

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -17,31 +17,33 @@ def amenity_get(place_id):
         abort(404)
     list_amenities = []
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        list_amenity = obj_place.amenities
-        for amenity in list_amenity:
-            list_amenities.append(amenity.to_dict)
+        for amenity in obj_place.amenities:
+            list_amenities.append(amenity.to_dict())
     else:
         for amenity in obj_place.amenity_ids:
-            list_amenities.append(storage.get(Amenity, amenity).to_dict)
+            list_amenities.append(amenity.to_dict())
     return jsonify(list_amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
-def amenity_post(place_id):
+def amenity_post(place_id, amenity_id):
     """returns a json file with the dictionary of a newly added object"""
     obj_place = storage.get(Place, place_id)
     obj_amenity = storage.get(Amenity, amenity_id)
-    if obj_place is None:
-        abort(404)
-    if obj_amenity is None:
+    if obj_place is None or obj_amenity is None:
         abort(404)
     exist = 0
-    for amenity in storage.all(Amenity).values():
-        if place.amenity.place_id == place_id and \
-         place.amenity_amenity_id == amenity_id:
-            return jsonify(amenity.to_dict()), 200
-    setattr(obj_place, amenity.amenity_id, amenity_id)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        if obj_amenity in obj_place.amenities:
+            return jsonify(obj_amenity.to_dict()), 200
+        else:
+            obj_place.amenities.append(obj_amenity)
+    else:
+        if obj_amenity in obj_place.amenity_ids:
+            return jsonify(obj_amenity.to_dict()), 200
+        else:
+            obj_place.amenity_ids.append(obj_amenity)
     storage.save()
     return jsonify(obj_amenity.to_dict()), 201
 
@@ -52,9 +54,7 @@ def amenity_delete(place_id, amenity_id):
     """returns a json file with an empty dictionary if successfully deleted"""
     obj_place = storage.get(Place, place_id)
     obj_amenity = storage.get(Amenity, amenity_id)
-    if obj_place is None:
-        abort(404)
-    if obj_amenity is None:
+    if obj_place is None or obj_amenity is None:
         abort(404)
     exist = 0
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
